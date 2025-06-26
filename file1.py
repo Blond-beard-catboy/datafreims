ba800c057c08137da6c33e5efd27df03ef82f183
Alex <paradoxlover13@gmail.com>
2025-06-26 20:25:43 +0500

    proper data added

--- file1.py
+++ file1.py
@@ -1,1 +1,10 @@
 import pandas as pd
+
+pd.set_option('display.max_rows', 20)      # Максимум строк
+pd.set_option('display.max_columns', 20)    # Максимум столбцов
+pd.set_option('display.width', 100)        # Ширина вывода
+
+df = pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]], columns=['A', 'B', 'C'], index=['x', 'y', 'z'])
+print(df)
+print("--------------------------------")
+print(df.describe())
