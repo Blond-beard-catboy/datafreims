77837437c06104f2e044400e78e4cdeabbde3a78
Alex <paradoxlover13@gmail.com>
2025-06-26 20:58:37 +0500

    examples of using loc and iloc

--- csv-read.py
+++ csv-read.py
@@ -1,1 +1,18 @@
 import pandas as pd
+
+coffee = pd.read_csv('./data/coffee.csv')
+print(coffee)
+print("\n")
+
+df1 = coffee.loc[:, ["Coffee Type", "Units Sold"]]
+print(df1)
+print("\n")
+
+df2 = coffee.loc[:, ["Day", "Units Sold"]]
+print(df2)
+print("\n")
+
+
+df3 = coffee.iloc[0:6, [0, 2]]
+print(df3)
+print("\n")
